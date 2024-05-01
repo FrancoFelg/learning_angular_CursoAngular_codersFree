85df569e789ca8614bd532b9e2207e0ffc821374
FrancoFelg <64760354+FrancoFelg@users.noreply.github.com>
2024-05-01 17:56:06 -0300

    17 - FormControl y Validators

--- curso_angular/src/app/app.module.ts
+++ curso_angular/src/app/app.module.ts
@@ -9,7 +9,9 @@ import { FormularioComponent } from './formulario/formulario.component';
 import { BuclesComponent } from './bucles/bucles.component';
 import { SwitchComponent } from './switch/switch.component';
 import { Formulario2Component } from './formulario2/formulario2.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Formulario3Component } from './formulario3/formulario3.component';
+
 
 @NgModule({
   declarations: [
@@ -19,11 +21,13 @@ import { FormsModule } from '@angular/forms';
     FormularioComponent,
     BuclesComponent,
     SwitchComponent,
-    Formulario2Component
+    Formulario2Component,
+    Formulario3Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
